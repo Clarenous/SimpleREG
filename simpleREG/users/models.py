0d29bb2240aa10f3d34952ce8378c0293b676410
GaoCeyu <gaoceyu@gmail.com>
2018-02-07 11:14:40 +0800

    2018.02.07.noon

--- simpleREG/users/models.py
+++ simpleREG/users/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     LoginFirstTime = models.BooleanField(default=True)
-    StuNumber = models.IntegerField()
-    StuName = models.CharField(max_length=50)
-    StuClass = models.IntegerField()
-    StuQQ = models.IntegerField()
+    StuNumber = models.IntegerField(default=0)
+    StuName = models.CharField(max_length=50, default="blank")
+    StuClass = models.IntegerField(default=0)
+    StuQQ = models.IntegerField(default=0)
 
     class Meta(AbstractUser.Meta):
         pass
