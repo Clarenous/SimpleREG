f7a9f3170284e4729f1fb35fb80535ea2ca3ac4d
GaoCeyu <gaoceyu@gmail.com>
2018-03-12 19:10:38 +0800

    2018.03.12.delete.DB

--- simpleREG/exeats/admin.py
+++ simpleREG/exeats/admin.py
@@ -2,19 +2,6 @@ from django.contrib import admin
 from .models import Holiday, LeaveInfo
 
 
-class LeaveInfoInline(admin.TabularInline):
-    model = LeaveInfo
-    extra = 1
+admin.site.register(Holiday)
 
-
-class HolidayAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['HolidayName']}),
-        ('Holiday ID', {'fields': ['HolidayID']}),
-        ('Holiday Start Date', {'fields': ['HolidayStartDate'], 'classes': ['collapse']}),
-        ('Holiday End Date', {'fields': ['HolidayEndDate'], 'classes': ['collapse']}),
-    ]
-    inlines = [LeaveInfoInline]
-
-
-admin.register(Holiday, LeaveInfo)
+admin.site.register(LeaveInfo)
--- simpleREG/exeats/models.py
+++ simpleREG/exeats/models.py
@@ -2,18 +2,17 @@ from django.db import models
 
 
 class Holiday(models.Model):
-    HolidayID = models.AutoField(primary_key=True, default=0)
     HolidayName = models.CharField(max_length=50, default="blank")
     HolidayStartDate = models.DateField(auto_now=False, auto_now_add=False, default=0)
     HolidayEndDate = models.DateField(auto_now=False, auto_now_add=False, default=0)
-    #def __str__(self):
-        #return self.HolidayID + ' ' + self.HolidayName + ' ' + self.HolidayStartDate + ' ' + self.HolidayEndDate
+
+    def __str__(self):
+        return str(self.id) + ' : ' + self.HolidayName + ' ' + str(self.HolidayStartDate) + ' ' + str(self.HolidayEndDate)
 
 
 class LeaveInfo(models.Model):
     holiday = models.ForeignKey(Holiday, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('date published')
-    LeaveInfoID = models.AutoField(primary_key=True, default=0)
     # Information we need as followed:
     StuClass_int = models.IntegerField(default=0)
     StuNumber_int = models.IntegerField(default=0)
@@ -24,3 +23,6 @@ class LeaveInfo(models.Model):
     StuPhone_bigint = models.BigIntegerField(default=0)
     Contact_text = models.CharField(max_length=50, default="blank")
     ContactPhone_bigint = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id) + ' : ' + self.StuName_text + str(self.StuNumber_int)
